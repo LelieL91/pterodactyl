d3fc1eeacecdd4a4dc2f041822aa9d32318ae927
LelieL91 <48968075+LelieL91@users.noreply.github.com>
2025-11-12 23:56:10 +0100

    update

---
+++ images/debian13-os/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Wait for the container to fully initialize
+sleep 1
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
